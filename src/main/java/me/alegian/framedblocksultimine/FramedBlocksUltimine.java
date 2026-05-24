e971e868749cc0c2910ff6b3d9ebf2fac31ad160
alegian <alegiannx@gmail.com>
2026-05-24 20:43:13 +0300

    empty project

---
+++ src/main/java/me/alegian/framedblocksultimine/FramedBlocksUltimine.java
@@ -0,0 +1,41 @@
+package me.alegian.framedblocksultimine;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(FramedBlocksUltimine.MODID)
+public class FramedBlocksUltimine {
+  public static final String MODID = "framed-blocks-ultimine";
+
+  public FramedBlocksUltimine(FMLJavaModLoadingContext context) {
+    IEventBus modEventBus = context.getModEventBus();
+
+    // Register the commonSetup method for modloading
+    modEventBus.addListener(this::commonSetup);
+
+    // Register ourselves for server and other game events we are interested in
+    MinecraftForge.EVENT_BUS.register(this);
+  }
+
+  private void commonSetup(final FMLCommonSetupEvent event) {
+  }
+
+  @SubscribeEvent
+  public void onServerStarting(ServerStartingEvent event) {
+  }
+
+  @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+  public static class ClientModEvents {
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+    }
+  }
+}
